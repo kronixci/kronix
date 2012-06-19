4d30583b7cd70ec927ca0452c38983232a643125
jhonnyquest <jonathan.duarte@rocketmail.com>
2012-06-19 15:30:44 -0300

    Unused variables

--- lib/kronix/rspec_engine.rb
+++ lib/kronix/rspec_engine.rb
@@ -2,7 +2,7 @@ module Kronix
   class RspecEngine
     def self.parse(end_line)
       pattern = /(\d+) examples?, (\d+) failures?(, (\d+) pending)?/
-      regex, test, fails, waiting, wait = *end_line.match(pattern)
+      _, test, fails, _, wait = *end_line.match(pattern)
       [test, fails, wait].map.collect(&:to_i)
     end
   end
