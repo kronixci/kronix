4c77d072eef8110d249de92ceb29579e82eeffcd
jhonnyquest <jonathan.duarte@rocketmail.com>
2012-06-19 16:07:40 -0300

    not show cloning result

--- lib/kronix/project.rb
+++ lib/kronix/project.rb
@@ -2,7 +2,7 @@ module Kronix
   class Project
     def self.clone(url, path)
       create_dir(path)
-      `cd #{path} && git clone #{url}`
+      `cd #{path} && git clone #{url} 2>&1`
     end
 
     def self.create_dir(path)
